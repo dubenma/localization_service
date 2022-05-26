ecac8deb5cb7e85b4155a682788cb8dfff19efb1
Martina Dubenova <dubenma1@fel.cvut.cz>
2022-05-26 18:51:49 +0200

    databaze for nn run parallely and with full sized images

--- SPRING_Demo/dataset_nn.m
+++ SPRING_Demo/dataset_nn.m
@@ -1,9 +1,10 @@
-clear all
+clear
 close all
 startup;
 
 %% set params 
 mode = "SPRING_Demo";
+% mode = "B315";
 setenv("INLOC_EXPERIMENT_NAME",mode)
 setenv("INLOC_HW","GPU")
 [ params ] = setupParams(mode, true);
@@ -21,20 +22,25 @@ t_max_far = 3;
 th_max_close = 15; 
 th_max_far = 180; 
 
+% should the images be resized?
+resized = false;
+
 %% generate data
 
 load(params.input.qlist.path);
 
 for i = 1 : 10 %length(query_imgnames_all)
-    output_dir = fullfile(output_path, mode, string(i));
+    output_dir = fullfile(output_path, mode, params.dataset.name, string(i));
     if ~exist(output_dir, 'dir')
         mkdir(output_dir)
     end
-    output_file = fullfile(output_dir, 'data.mat');
+    
+    output_file = fullfile(output_dir, 'query.mat');
+    
     if ~(exist(output_file) == 2)
         qname = query_imgnames_all{i};
         spaceName = strsplit(qname,'/'); spaceName = spaceName{1};
-        [~,query_id,~] = fileparts(qname); query_id = str2num(query_id); % space_id is the query id
+        [~,query_id,~] = fileparts(qname); query_id = str2double(query_id);
 
         run(fullfile(params.dataset.query.mainDir, spaceName, 'query_all', 'metadata', 'query_mapping.m'));
         trueName = q2name(query_id);
@@ -46,14 +52,23 @@ for i = 1 : 10 %length(query_imgnames_all)
         end
 
         % query
-        iq = imread(fullfile(params.dataset.query.mainDir , qname));
-        Iq = imresize(iq, params.dataset.query.dslevel);
+        Iq = imread(fullfile(params.dataset.query.mainDir , qname));
+        if resized
+            Iq = imresize(Iq, params.dataset.query.dslevel);
+        end
     %     figure();imshow(Iq)
     %     figure();imshow(fullfile(params.dataset.query.mainDir, spaceName, 'cutouts', string(panoDirId), trueName));
 
         % query mask
-        mask_name = fullfile(params.input.dir, "queries_masks", string(i) + ".png");
-        mask = imresize(imread(mask_name), params.dataset.query.dslevel, 'nearest');
+        if strcmp(params.dynamicMode, 'original') % staticky, dataset, takze masky neexistuju
+            mask = zeros(size(Iq));
+        else
+            mask_name = fullfile(params.input.dir, "queries_masks", string(i) + ".png");
+            mask = imread(mask_name);
+        end
+        if resized
+            imresize(mask, params.dataset.query.dslevel, 'nearest');
+        end
     %     figure();imshow(mask)
     %     figure();imshow([rgb2gray(Iq) mask]);
 
@@ -62,7 +77,13 @@ for i = 1 : 10 %length(query_imgnames_all)
         ref_pose = load(pose_path);
 
         meshPath = fullfile(params.dataset.models.dir, spaceName, 'model.obj');
-        fl = params.camera.fl * params.dataset.query.dslevel;
+        if strcmp(mode, 'B315')
+            meshPath = fullfile(params.dataset.models.dir, 'b315', 'model.obj');
+        end
+        fl = params.camera.fl;
+        if resized
+            fl = fl * params.dataset.query.dslevel;
+        end
 
         sensorSize = [size(Iq,2), size(Iq,1)];
 
@@ -72,19 +93,20 @@ for i = 1 : 10 %length(query_imgnames_all)
         Rfix = rotationMatrix(deg2rad(rFix), 'XYZ');
         new_pose.R =Rfix*ref_pose.R';
 
-        data = struct();
-        data.query.true_name = trueName;
-        data.query.query_img = Iq;
-        data.query.mask = mask;
-        data.query.qname = qname;
-        data.query.R = new_pose.R';
-        data.query.C = ref_pose.position';
+        query = struct();
+        query.true_name = trueName;
+        query.query_img = Iq;
+        query.mask = mask;
+        query.qname = qname;
+        query.R = new_pose.R';
+        query.C = ref_pose.position';
 
-        data.synth = cell(total_n,1);
+        parfor j = 1 : total_n
 
-        for j = 1 : total_n
-
-            if j <= close_n
+            if j == 1
+                t_max = 0;
+                th_max = 0;
+            elseif j <= close_n
                 t_max = t_max_close;
                 th_max = th_max_close; 
             else
@@ -103,32 +125,46 @@ for i = 1 : 10 %length(query_imgnames_all)
             th = rand(1) * th_max / 180 * pi;
             x_ = @(v) [0 -v(3) v(2); v(3) 0 -v(1); -v(2) v(1) 0];
             R = (1-cos(th)) * v*v' + cos(th) * eye(3) + sin(th) * x_(v);
-
-            % new rot and trans 
-            rot = R*new_pose.R';
-            trans = ref_pose.position' + t;
+            
+            if j == 1
+                rot = new_pose.R';
+                trans = ref_pose.position';              
+            else
+                rot = R*new_pose.R';
+                trans = ref_pose.position' + t;
+            end
 
             error_rotation = rotationDistance(new_pose.R', rot);
             error_translation = norm(ref_pose.position' - trans);
 
-            data.synth{j}.R = rot;
-            data.synth{j}.C = trans;
-            data.synth{j}.error_rotation = error_rotation;
-            data.synth{j}.error_translation = error_translation;
-            data.synth{j}.added_translation = t;
-            data.synth{j}.added_rotation.angle = th;
-            data.synth{j}.added_rotation.axis = v;
-            data.synth{j}.added_rotation.R = R;
+            synth = struct();
+            synth.R = rot;
+            synth.C = trans;
+            synth.error_rotation = error_rotation;
+            if j == 1
+                synth.error_rotation = 0;
+            end
+            synth.error_translation = error_translation;
+            synth.added_translation = t;
+            synth.added_rotation.angle = th;
+            synth.added_rotation.axis = v;
+            synth.added_rotation.R = R;
 
             [RGBpersp, XYZpersp, depth] = projectMesh(meshPath, fl, rot, trans, sensorSize, false, -1, params.input.projectMesh_py_path, -1);
-            data.synth{j}.synth_img = RGBpersp;
+            synth.synth_img = RGBpersp;
     %     figure();
     %     imshow(RGBpersp);
     %     figure();
     %     imshow(Iq);
     %     figure();
     %     imshow(mask);
+        output_synth_file = fullfile(output_dir, ['synth',num2str(j),'.mat']);
+        parsavesynth(output_synth_file, synth)
         end
-        save(output_file, 'data');
+        save(output_file, 'query');
     end
+end
+
+function parsavesynth(path, synth)
+  save(path, 'synth')
 end
